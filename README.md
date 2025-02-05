d1bc0150c6d3fd71e273c1cb7436498affaefb67
Mikhail Vays <m.vais@g.nsu.ru>
2025-02-05 06:23:09 +0000

    Added task1 implementation with cmake build configuration

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# nsu_concurrency_theory
+## Как выбрать тип массива?
+
+для double (по умолчанию) 
+> cmake -B build .
+
+для float
+> cmake -B build . -DUSE_FLOAT=ON
+
+Затем сборка и запуск
+> cmake --build build
+
+> ./build/task1
+
+## Результаты
+
+> float: -0.0277862
+
+> double: 3.68912e-10
---
+++ task1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <typeinfo>
+
+#define ARRAY_SIZE 10000000
+// acos(0.0) will return the value for Pi/2. 
+// To get the value of Pi we need to multiply it on 2:
+#define PI (2*acos(0.0))
+#define STEP_SIZE (2 * PI / ARRAY_SIZE)
+
+#if FLOAT
+    #define ARRAY_TYPE float
+#else
+    #define ARRAY_TYPE double
+#endif
+
+int main() {
+    ARRAY_TYPE sum = 0;
+    std::vector<ARRAY_TYPE> array(ARRAY_SIZE);
+
+    std::cout << typeid(ARRAY_TYPE).name() << std::endl;
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = sin(static_cast<ARRAY_TYPE>(i) * STEP_SIZE);
+        sum += array[i];
+    }
+
+    std::cout << "Sum: " << sum << std::endl;
+    return 0;
+}
