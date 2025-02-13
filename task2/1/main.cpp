0f739a8f954adaacbcb558e94a73b62e872f5dd7
Mikhail Vays <m.vais@g.nsu.ru>
2025-02-13 14:50:42 +0000

    added plotting function && serial matrix multiplication

--- task2/1/main.cpp
+++ task2/1/main.cpp
@@ -3,6 +3,10 @@
 #include <time.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <vector>
+
+#include "pbPlots.hpp"
+#include "supportLib.hpp"
 
 #define NUM_THREADS 8
 #define MATRIX_SIZE 20000
@@ -22,6 +26,17 @@ void matrix_vector_product_omp(double *a, double *b, double *c, const int m, con
             for (int j = 0; j < n; j++)
                 c[i] += a[i * n + j] * b[j];
         }
+    }
+}
+
+/*
+* matrix_vector_product: Compute matrix-vector product c[m] = a[m][n] * b[n]
+*/
+void matrix_vector_product(double *a, double *b, double *c, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        c[i] = 0.0;
+        for (int j = 0; j < n; j++)
+            c[i] += a[i * n + j] * b[j];
     }
 }
 
@@ -58,10 +73,63 @@ void run_parallel(const int m, const int n) {
     free(c);
 }
 
+void run_serial(const int m, const int n) {
+    double *a, *b, *c;
+    a = (double*)malloc(sizeof(double) * m * n);
+    b = (double*)malloc(sizeof(double) * n);
+    c = (double*)malloc(sizeof(double) * m);
+
+    double t1 = omp_get_wtime();
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            a[i * n + j] = i + j;
+    }
+    for (int j = 0; j < n; j++)
+        b[j] = j;
+    printf("Elapsed allocation time (serial): %.2f sec.\n", omp_get_wtime()-t1);
+
+    double t = omp_get_wtime();
+    matrix_vector_product(a, b, c, m, n);
+    printf("Elapsed time (serial): %.2f sec.\n", omp_get_wtime()-t);
+
+    free(a);
+    free(b);
+    free(c);
+}
+
+void render_graph(std::vector<double> &x, std::vector<double> &y) {
+    RGBABitmapImageReference *imageRef = CreateRGBABitmapImageReference();
+    StringReference *errorMessage = CreateStringReferenceLengthValue(0, L' ');
+
+    bool result = DrawScatterPlot(imageRef, 600, 400, &x, &y, errorMessage);
+
+    if (result) {
+        std::vector<double> *pngData = ConvertToPNG(imageRef->image);
+        WriteToFile(pngData, "./plot.png");
+        DeleteImage(imageRef->image);
+    } else {
+        std::cerr << "Error: ";
+		for(wchar_t c : *errorMessage->string){
+			std::wcerr << c;
+		}
+		std::cerr << std::endl;
+    }
+
+    FreeAllocations();
+}
+
 int main(int argc, char **argv) {
     const int m = MATRIX_SIZE, n = MATRIX_SIZE;
+    
     printf("Matrix-vector product (c[m] = a[m, n] * b[n]; m = %d, n = %d)\n", m, n);
     printf("Memory used: %" PRIu64 " MiB\n", ((m * n + m + n) * sizeof(double)) >> 20);
+    
+    run_serial(m, n);
     run_parallel(m, n);
+    
+    std::vector<double> x{-2, -1, 0, 1, 2};
+    std::vector<double> y{2, -1, -2, -1, 2};
+    render_graph(x, y);
+    
     return 0;
 }
