87cf365942d9192df3f7bd500e41b49e1f54a1e1
Mikhail Vays <m.vais@g.nsu.ru>
2025-02-17 10:06:43 +0000

    added graphs for task3

---
+++ task2/graphs/3/build_graphs.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+
+def read_data(filename):
+    threads, times, speedups = [], [], []
+    with open(filename) as f:
+        for line in f:
+            cols = line.split()
+            threads.append(int(cols[0]))
+            times.append(float(cols[1]))
+            speedups.append(float(cols[2]))
+    return threads, times, speedups
+
+def plot_and_save(threads, times, speedups, filename_prefix):
+    plt.figure(figsize=(10, 5))
+    
+    plt.subplot(1, 2, 1)
+    plt.plot(threads, times, marker='o', label="Время")
+    plt.xlabel("Число потоков")
+    plt.ylabel("Время (сек)")
+    plt.grid(True)
+    plt.legend()
+    plt.title(f"{filename_prefix}: Время выполнения")
+    plt.savefig(f"{filename_prefix}_time.png")
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(threads, speedups, marker='s', label="Ускорение")
+    plt.xlabel("Число потоков")
+    plt.ylabel("Ускорение")
+    plt.grid(True)
+    plt.legend()
+    plt.title(f"{filename_prefix}: Ускорение")
+    plt.savefig(f"{filename_prefix}_speedup.png")
+    
+    plt.show()
+
+for filename in ["task31.txt", "task32.txt"]:
+    threads, times, speedups = read_data(filename)
+    plot_and_save(threads, times, speedups, filename.split('.')[0])
